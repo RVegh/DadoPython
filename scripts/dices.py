950c4d6af3fe3330d5247951ced52f1fe5871582
RVegh <60322449+RVegh@users.noreply.github.com>
2021-03-01 23:42:44 -0300

    Added d20 function and variable changes

---
+++ scripts/dices.py
@@ -0,0 +1,17 @@
+import numpy as np
+import random
+
+#Create a random number from 1 to 6 -- D6
+def dado_d6(lados_d6):
+    for lados_d6 in range(1):
+        print(random.randint(1,6))
+
+dado_d6(1)
+
+
+#Create a random number from 1 to 20 -- D20
+def dado_d20(lados_d20):
+    for lados_d20 in range(1):
+        print(random.randint(1,20))
+
+dado_d20(1)
