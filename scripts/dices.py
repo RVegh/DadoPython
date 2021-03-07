41fb585b403292c52c79776c529bb4241bb33105
RVegh <60322449+RVegh@users.noreply.github.com>
2021-03-07 00:33:09 -0300

    editing dice function

--- scripts/dices.py
+++ scripts/dices.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 
-#Create a random number from 1 to 6 -- D6
+#Create a random number from 1 to 6 -- D6, D20, D100
 def dado_d6(lados_d6):
     for lados_d6 in range(1):
         print(random.randint(1,6))
@@ -9,9 +9,14 @@ def dado_d6(lados_d6):
 dado_d6(1)
 
 
-#Create a random number from 1 to 20 -- D20
 def dado_d20(lados_d20):
     for lados_d20 in range(1):
         print(random.randint(1,20))
 
 dado_d20(1)
+
+def dado_d100(lados_d100):
+    for lados_d100 in range(1):
+        print(random.randint(1,100))
+
+dado_d100(1)
